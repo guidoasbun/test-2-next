82356d2d660b73e2c7c65827a0a29408b9c972ff
Guido Asbun <guido@asbun.io>
2024-03-27 17:58:35 -0700

    test 3

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Test App 2</h1>
       <h1 className="text-4xl font-bold">Branch Test 2</h1>
+      <h1 className="text-4xl font-bold">Branch Test 3</h1>
     </main>
   );
 }
